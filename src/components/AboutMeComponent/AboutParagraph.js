398b304c797f97507f3f0fdfdf85eaa4985d3316
Albert Hu <hualbert@rocketmail.com>
2022-11-25 17:21:23 -0800

    fixed wording

--- src/components/AboutMeComponent/AboutParagraph.js
+++ src/components/AboutMeComponent/AboutParagraph.js
@@ -46,10 +46,10 @@ const App = () => {
                 This had led me to the journey of building web applications as a front end developer, which allows me to synergize my technical acumen and my creativity together to build remarkable things. And most importantly, it allows me to scale and maximize the benefits I can provide to a community.  
             </Paragraph>
             <Paragraph className = "About_paragraph">
-                I believe that one must constantly strive to live outside their comfort zone and improve on his craft. He can only grow and become the best version of himself by living in the edge. 
+                I believe that one must constantly strive to live outside their comfort zone and improve on his craft. A person can only grow and become the best version of himself by living in the edge. 
             </Paragraph>
             <Paragraph className ="About_paragraph">
-                The journey to realize his true potential is like climbing a muddy hill. If he stays still, he slides back and loses the progress he has made. That is why he must constantly put in the work to climb to the peak. This is why I am on the path of constant improvement.
+                The journey to realize one's true potential is like climbing a muddy hill. If he stays still, he slides back and loses the progress he has made. That is why he must constantly put in the work to climb to the peak. This is why I am on the path of constant improvement.
                 </Paragraph>
 
         </ParaWrapper>
