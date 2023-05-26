b7d602dfb8e6cfc213b9e14c824362fc7aa7d8da
unknown <hualbert@rocketmail.com>
2023-05-25 20:17:10 -0700

    saving latest changes

---
+++ src/screens/homepage/textComponents.js
@@ -0,0 +1,33 @@
+const BlabberParagraphs = ["Blabber is a social media platform that allows users to speak their mind. It gives users many features found in prevalent social media sites such as Facebook and Instagram to play around with. It communicates with an API that is built with Node JS. In addition, the server uses RESTful and MVC design patterns to save and manage data in Mongo DB. Json Web Tokens are used to authenticate users."]
+const EarthToneParagraphs = ["Earth Tone is an E-Commerce site that sells tea products and is capable of handling real financial transactions. It has all the basic E-Commerce functionalities including, but not limited to creating an authenticated personal shopping accounting, finding products with the search bar, adding products to a cart/wish list, and creating an order. In addition, the site has an optimized user experience created with intuitive UX design practices, resulting in smooth and efficient navigation.",
+    "It has all the basic eCommerce functionalities including, but not limited to:"];
+
+const RedditParagraphs = [
+    "This is a project where I cloned my favorite website Reddit using React JS and it is integrated with Firebase.",
+    "It has all the basic features of Reddit including, but not limited to:",
+]
+
+const EarthToneList = [
+    "Product Pages",
+    "Search functionality",
+    "Creating a personal account",
+    "Credit cart functionality",
+    "Order processing",
+]
+
+const RedditList = [
+    "Creating a profile",
+    "Creating threads and being able to respond to them",
+    "Voting on thread and comments",
+    "Filtering thread feeds and comment feeds based on date, popularity and controversy",
+    "Toggling the display theme of each of the communities ",
+]
+
+
+export {
+    BlabberParagraphs,
+    EarthToneParagraphs,
+    RedditParagraphs,
+    EarthToneList,
+    RedditList,
+}
